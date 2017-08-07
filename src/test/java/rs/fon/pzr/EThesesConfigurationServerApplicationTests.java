ed6cec938412c9fc17d56a0c6974e78e9c5cb330
= <jovic.i24@gmail.com>
2017-08-07 22:17:34 +0200

    implement configuration server

---
+++ src/test/java/rs/fon/pzr/EThesesConfigurationServerApplicationTests.java
@@ -0,0 +1,16 @@
+package rs.fon.pzr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EThesesConfigurationServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
